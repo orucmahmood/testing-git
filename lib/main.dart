c69c3cb52c24fc3949c02ce09ef693ac80cb7441
Oruj Mahmud <orucmahmood@gmail.com>
2025-12-26 11:08:24 +0400

    son dəyişikliklər

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mənim İlk Git Layihəm',
+      title: 'Mənim İlk Git Layihəm - Yeni dəyişiklik',
       theme: ThemeData(
         // This is the theme of your application.
         //
